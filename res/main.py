92aad08294e602bf56d2d706bfc7bff9e7fa584f
Andrey_Kot <andrey42kot@gmail.com>
2023-10-05 21:28:18 +0700

    Обновил стартовое расписание, сделал чтобы при отсутствии занятий на неделе, об это сообщалось

--- res/main.py
+++ res/main.py
@@ -15,6 +15,7 @@ from timechecker import timetable_on_date
 # Объект бота
 TOKEN = config.bot_token.get_secret_value()
 
+
 bot = Bot(token=TOKEN)
 # Диспетчер
 dp = Dispatcher(bot, storage=MemoryStorage())
@@ -77,7 +78,7 @@ async def start(msg: types.Message):
         # Отправляем текст с предложением ввести класс
         await bot.send_message(
             msg.from_user.id,
-            START_TEXT, reply_markup=types.ReplyKeyboardRemove())
+            START_TEXT, reply_markup=types.ReplyKeyboardRemove(), parse_mode=types.ParseMode.HTML)
 
         _temp = None
 
@@ -243,6 +244,8 @@ async def main_menu(msg: types.Message):
 
         tt = {}
 
+        has_schedule = False
+
         for i in range(7):
             new_date = start_date + timedelta(days=i)
             events = timetable_on_date(new_date.date(), cursor)
@@ -269,11 +272,17 @@ async def main_menu(msg: types.Message):
                 if user_data[0] in cl and name in user_data[1]:
                     temp_output += f"{' ' * 7}• {name}\n{' ' * 10}" +\
                         f"Время: {ts} - {te}\n\n"
+                    # Устанавливаем флаг has_schedule в True, так как есть расписание
+                    has_schedule = True
 
             tt[wd] = temp_output[:-1]
-
         sep = "—" * 8
-        send_text += sep.join([e + tt[e] for e in tt if tt[e]])
+        if has_schedule:
+            send_text += sep.join([e + tt[e] for e in tt if tt[e]])
+        else:
+            send_text = "На этой неделе у вас нет занятий. Обучение стартует с 9 октября"
+        
+        #send_text += sep.join([e + tt[e] for e in tt if tt[e]])
 
         await bot.send_message(msg.from_user.id, send_text)
 
